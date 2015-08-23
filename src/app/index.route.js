be55efafecbba1ace71d8b3b176882195edaa36b
Gergely Lato <gergely.lato@oce.com>
2015-08-23 23:33:58 +0200

    Added all routes and logo

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -20,6 +20,72 @@
             controllerAs: 'main'
           }
         }
+      })
+      .state('introduction', {
+        url: '/bemutatkozas',
+        views: {
+          "header": {
+            templateUrl: 'app/main/main.header.html'
+          },
+          "content": {
+            templateUrl: 'app/components/introduction/introduction.html'
+          }
+        }
+      })
+      .state('nails', {
+        url: '/korom',
+        views: {
+          "header": {
+            templateUrl: 'app/main/main.header.html'
+          },
+          "content": {
+            templateUrl: 'app/components/nails/nails.html'
+          }
+        }
+      })
+      .state('makeup', {
+        url: '/smink',
+        views: {
+          "header": {
+            templateUrl: 'app/main/main.header.html'
+          },
+          "content": {
+            templateUrl: 'app/components/makeup/makeup.html'
+          }
+        }
+      })
+      .state('prices', {
+        url: '/arak',
+        views: {
+          "header": {
+            templateUrl: 'app/main/main.header.html'
+          },
+          "content": {
+            templateUrl: 'app/components/prices/prices.html'
+          }
+        }
+      })
+      .state('contact', {
+        url: '/kapcsolat',
+        views: {
+          "header": {
+            templateUrl: 'app/main/main.header.html'
+          },
+          "content": {
+            templateUrl: 'app/components/contact/contact.html'
+          }
+        }
+      })
+      .state('news', {
+        url: '/ujdonsag',
+        views: {
+          "header": {
+            templateUrl: 'app/main/main.header.html'
+          },
+          "content": {
+            templateUrl: 'app/components/news/news.html'
+          }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,70 +6,62 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, toastr) {
+  function MainController() {
     var vm = this;
-
-    vm.new = [{
-      img: '/assets/images/new/1.jpg'
-    }, {
-      img: '/assets/images/new/2.jpg'
-    }, {
-      img: '/assets/images/new/3.jpg'
-    }]
 
     vm.$shadow = $('#shadow').hide();
     vm.$slicebox = $('#sb-slider').slicebox({
-            // (v)ertical, (h)orizontal or (r)andom
-            orientation: 'v',
+      // (v)ertical, (h)orizontal or (r)andom
+      orientation: 'v',
 
-            // perspective value
-            perspective: 1200,
+      // perspective value
+      perspective: 1200,
 
-            // number of slices / cuboids
-            // needs to be an odd number 15 => number > 0 (if you want the limit higher,
-            // change the _validate function).
-            cuboidsCount: 4,
+      // number of slices / cuboids
+      // needs to be an odd number 15 => number > 0 (if you want the limit higher,
+      // change the _validate function).
+      cuboidsCount: 4,
 
-            // if true then the number of slices / cuboids is going to be random (cuboidsCount is overwitten)
-            cuboidsRandom: false,
+      // if true then the number of slices / cuboids is going to be random (cuboidsCount is overwitten)
+      cuboidsRandom: false,
 
-            // the range of possible number of cuboids if cuboidsRandom is true
-            // it is strongly recommended that you do not set a very large number :)
-            maxCuboidsCount: 5,
+      // the range of possible number of cuboids if cuboidsRandom is true
+      // it is strongly recommended that you do not set a very large number :)
+      maxCuboidsCount: 5,
 
-            // each cuboid will move x pixels left / top (depending on orientation).
-            // The middle cuboid doesn't move. the middle cuboid's neighbors will
-            // move disperseFactor pixels
-            disperseFactor: 0,
+      // each cuboid will move x pixels left / top (depending on orientation).
+      // The middle cuboid doesn't move. the middle cuboid's neighbors will
+      // move disperseFactor pixels
+      disperseFactor: 0,
 
-            // color of the hidden sides
-            colorHiddenSides: '#222',
+      // color of the hidden sides
+      colorHiddenSides: '#222',
 
-            // the animation will start from left to right. The left most
-            // cuboid will be the first one to rotate
-            // this is the interval between each rotation in ms
-            sequentialFactor: 150,
+      // the animation will start from left to right. The left most
+      // cuboid will be the first one to rotate
+      // this is the interval between each rotation in ms
+      sequentialFactor: 100,
 
-            // animation speed
-            // this is the speed that takes "1" cuboid to rotate
-            speed: 600,
+      // animation speed
+      // this is the speed that takes "1" cuboid to rotate
+      speed: 400,
 
-            // transition easing
-            easing: 'ease',
+      // transition easing
+      easing: 'ease',
 
-            // if true the slicebox will start the animation automatically
-            autoplay: true,
+      // if true the slicebox will start the animation automatically
+      autoplay: true,
 
-            // time (ms) between each rotation, if autoplay is true
-            interval: 2000,
+      // time (ms) between each rotation, if autoplay is true
+      interval: 2500,
 
-            // the fallback will just fade out / fade in the items
-            // this is the time fr the fade effect
-            fallbackFadeSpeed: 300,
-            onReady: function() {
-                vm.$shadow.show();
-            },
-            onBeforeChange: function() {}
-        });
+      // the fallback will just fade out / fade in the items
+      // this is the time fr the fade effect
+      fallbackFadeSpeed: 300,
+      onReady: function () {
+        vm.$shadow.show();
+      },
+      onBeforeChange: function () {}
+    });
   }
 })();
